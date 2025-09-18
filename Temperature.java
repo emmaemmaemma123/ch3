0951098d87fdffff59c07c8a2e9f5e9c54cb5b26
emmaemmaemma123 <emma.le.song@gmail.com>
2025-09-18 10:56:47 -0700

    Add files via upload

--- Temperature.java
+++ Temperature.java
@@ -1,11 +1,14 @@
+package ch3;
+
 import java.util.Scanner;
 public class Temperature {
     public static void main(String arngs[]){
         Scanner in = new Scanner(System.in);
         System.out.print("Enter a Temperature in Celsius: ");
         int celsius = in.nextInt();
+        double calculate = (double) (celsius*9)/5;
         
-        double fahrenheit = celsius * 9/5 + 32;
+        double fahrenheit = calculate + 32;
         
         System.out.print(celsius + "˚C = ");
         System.out.print(fahrenheit + "˚F");
